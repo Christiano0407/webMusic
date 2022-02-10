9fd2ff3b7e8a32a22f44506508aaaafccb274616
Christiano0407 <placebo0704@gmail.com>
2022-02-10 12:17:34 -0600

    gsap-animation part 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
+// == Gsap Animation"
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.fromTo(".circle", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.5 });
+tl.fromTo(".circle", { opacity: 0 }, { opacity: 1 });
+tl.fromTo(".clip", { opacity: 0 }, { opacity: 1 });
+
+tl.fromTo(".page", { opacity: 0 }, { opacity: 1 });
 
 tl.fromTo(
   ".navigation",
-  { y: "-200px", opacity: 0 },
-  { y: "0px", opacity: 1, delay: 0.5, duration: 0.5 }
+  { y: -200, opacity: 0 },
+  { y: 0, opacity: 1, delay: 0.5, duration: 0.5 },
+  "<"
 );
 tl.to(".main__text-one", {
   x: "-25%",
@@ -14,24 +19,34 @@ tl.to(".main__text-one", {
 });
 tl.fromTo(
   ".img-one",
-  { y: "-500px", opacity: 0 },
-  { y: "-50px", opacity: 1, delay: 1, duration: 1 }
+  { y: 0, opacity: 0 },
+  { y: -100, opacity: 1, delay: 1, duration: 1 },
+  "<"
 );
 tl.fromTo(
   ".text-twoTitle",
-  { x: "-300px", opacity: 0 },
-  { x: "0", opacity: 1, delay: 1, duration: 1 }
+  { x: -300, opacity: 0 },
+  { x: 0, opacity: 1, delay: 1, duration: 1 }
 );
 tl.fromTo(
   ".textSpan",
-  { x: "-200", opacity: 0 },
-  { x: "0px", opacity: 1, delay: 1.5, duration: 1, stagger: 0.5 },
+  { x: -200, opacity: 0 },
+  { x: 0, opacity: 1, delay: 1.5, duration: 1, stagger: 0.5 },
   "-=1"
 );
 tl.to(".textNum", { y: "5px", duration: 1 }, "-=1");
 
 tl.fromTo(
   ".btn__click",
-  { x: "0px", opacity: 0 },
-  { x: "-30px", opacity: 1, delay: 0.5, duration: 0.5 }
+  { x: 0, opacity: 0 },
+  { x: -30, opacity: 1, delay: 0.5, duration: 0.5 },
+  "<"
+);
+tl.from(".two-text", { x: "300px", delay: 1, duration: 1 }, "<");
+tl.fromTo(".two-list", { opacity: 0, scale: 0 }, { opacity: 1, scale: 1 }, "<");
+tl.fromTo(
+  ".two-images",
+  { x: 300, opacity: 0 },
+  { x: 0, opacity: 1, delay: 1, duration: 1 },
+  "<"
 );
